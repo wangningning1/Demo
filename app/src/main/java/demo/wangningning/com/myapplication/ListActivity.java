2b54e59f311b8799a14d3cf3e6214c9a53b1a753
jinpengfei <jinpengfei1989@gmail.com>
2018-01-06 18:08:47 +0800

    Add recyclerview

--- app/src/main/java/demo/wangningning/com/myapplication/ListActivity.java
+++ app/src/main/java/demo/wangningning/com/myapplication/ListActivity.java
@@ -2,9 +2,12 @@ package demo.wangningning.com.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ArrayAdapter;
-import  android.widget.ListView;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -15,23 +18,29 @@ import java.util.List;
  */
 
 public class ListActivity extends AppCompatActivity {
-    private RecyclerView rececyclerView;
-    private List<String> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_list );
-        rececyclerView = (RecyclerView)findViewById( R.id.recyclerView );
-        intiData();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
-        LinearLayout layout = new LinearLayout( this );
+        MyRecyclerAdapter recycleAdapter = new MyRecyclerAdapter(ListActivity.this, intiData());
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this,3);
+        layoutManager.setOrientation(OrientationHelper.VERTICAL);//设置为垂直布局，这也是默认的
+
+        recyclerView.setLayoutManager(layoutManager);//设置布局管理器
+        recyclerView.setAdapter(recycleAdapter);//设置Adapter
+        recyclerView.setItemAnimator(new DefaultItemAnimator());//设置增加或删除条目的动画
     }
-    private void intiData(){
-        mDatas = new ArrayList<String>(  );
-        for ( int i = 0;i < 100;i++){
-            mDatas.add("list"+i);
+
+    private List<String> intiData() {
+        List<String> testData = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            testData.add("list" + i);
         }
+        return testData;
     }
 
 }
--- app/src/main/java/demo/wangningning/com/myapplication/MyRecyclerAdapter.java
+++ app/src/main/java/demo/wangningning/com/myapplication/MyRecyclerAdapter.java
@@ -14,44 +14,38 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.My
 
     private List<String> mDatas;
     private Context mContext;
-    private LayoutInflater inflater;
+    private LayoutInflater mInflater;
 
     public MyRecyclerAdapter(Context context, List<String> datas) {
         this.mContext = context;
         this.mDatas = datas;
-        inflater = LayoutInflater.from( mContext );
+        mInflater = LayoutInflater.from(mContext);
     }
 
     @Override
     public int getItemCount() {
-
         return mDatas.size();
     }
 
     //填充onCreateViewHolder方法返回的holder中的控件
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
-
-        holder.tv.setText( mDatas.get( position ) );
+        holder.tv.setText(mDatas.get(position));
     }
 
     //重写onCreateViewHolder方法，返回一个自定义的ViewHolder
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View view = inflater.inflate( R.layout.item_home, parent, false );
-        MyViewHolder holder = new MyViewHolder( view );
+        View view = mInflater.inflate(R.layout.list_item, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
         return holder;
     }
 
     class MyViewHolder extends ViewHolder {
-
         TextView tv;
-
         public MyViewHolder(View view) {
-            super( view );
-            tv = (TextView) view.findViewById( R.id.tv_item );
+            super(view);
+            tv = (TextView) view.findViewById(R.id.tv_item);
         }
-
     }
 }
